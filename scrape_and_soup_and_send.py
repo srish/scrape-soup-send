dc347c677b62c859a865cf9fdbf6810baa884f0b
srish <srishakatux@gmail.com>
2018-05-02 19:41:44 -0700

    Pylint fixes + var declaration issue fix

--- scrape_and_soup_and_send.py
+++ scrape_and_soup_and_send.py
@@ -1,55 +1,67 @@
-# #!/usr/bin/env python
-from selenium import webdriver  
-from selenium.common.exceptions import NoSuchElementException  
-from selenium.webdriver.common.keys import Keys  
-from bs4 import BeautifulSoup
-from datetime import datetime
-from twilio.rest import Client
-import schedule
+#!/usr/bin/env python
+"""
+    Scrapes the appointment page of a visa website and
+    notifies of an early availability than the usual booking!
+
+    MIT license
+
+    :author: Srishti Sethi <ssethi@wikimedia.org>
+"""
+
 import time
+import schedule
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from twilio.rest import Client
 
-#Twilio credentials..obtain from https://www.twilio.com/console
-twilio_account_sid = 'enter_account_sid'
-twilio_auth_token = 'enter_auth_token'
-twilio_from_num = 'from_number'
-twilio_to_num = 'to_number'
+# Twilio credentials..obtain from https://www.twilio.com/console
+TWILIO_ACCOUNT_SID = 'enter_account_sid'
+TWILIO_AUTH_TOKEN = 'enter_auth_token'
+TWILIO_FROM_NUM = 'from_number'
+TWILIO_TO_NUM = 'to_number'
 
-#visa appointment details 
-targeturl = 'enter_the_url_here'
-i_am_going_to_Barcelona_on = "Day MM dd YYYY"
+# visa appointment details
+TARGET_URL = 'enter_the_url_here'
+I_AM_GOING_TO_BARCELONA_ON = "Day MM dd YYYY"
 
-def scrapeSelectedDate(url): 
-	browser = webdriver.Firefox(executable_path=r'/path_to_geckodriver')
-	browser.get(url)   
-	time.sleep(50)  #50 is too much..but the page I'm trying to scrape loads quite slow
-	html_source = browser.page_source  
-	soup = BeautifulSoup(html_source,'html.parser') 
 
-	#For debugging purpose only
-	# with open("Output.html", "w") as text_file:
-	# 	text_file.write(str(soup))
+def scrape_selected_date(url):
+    """ Scrapes the appointment date on the appointment page
+    """
+    browser = webdriver.Firefox(executable_path=r'/path_to_geckodriver')
+    browser.get(url)
+    # 50 is too much..but the page I'm trying to scrape loads quite slow
+    time.sleep(50)
+    html_source = browser.page_source
+    soup = BeautifulSoup(html_source, 'html.parser')
+    date = soup.find(id="idDivBktDatetimeSelectedDate").text
+    browser.quit()
+    return date
 
-	date = soup.find(id="idDivBktDatetimeSelectedDate").text
-	browser.quit()
-	return date
 
-def sendMsg(diff, date, msg):
-	client = Client(twilio_account_sid, twilio_auth_token)
-	client.messages.create(from_=twilio_from_num, 
-		to=twilio_to_num, 
-		body=msg)
+def send_msg(msg):
+    """ Sends message via Twilio
+    """
+    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    client.messages.create(from_=TWILIO_FROM_NUM, to=TWILIO_TO_NUM, body=msg)
 
-def checkIfThereIsAnAvailability():
-    date = scrapeSelectedDate(targeturl)
-    firstAptAvail = datetime.strptime(date, '%A %B %d %Y')
-    iAmGoingOn = datetime.strptime(i_am_going_to_Barcelona_on, '%A %b %d %Y')
-    diff = (iAmGoingOn - firstAptAvail).days
+
+def check_for_availability():
+    """ Check if there is an availability few weeks prior to the travel
+    """
+    date = scrape_selected_date(TARGET_URL)
+    first_apt_avail = datetime.strptime(date, '%A %B %d %Y')
+    i_am_going_on = datetime.strptime(I_AM_GOING_TO_BARCELONA_ON, '%A %b %d %Y')
+    diff = (i_am_going_on - first_apt_avail).days
     if diff > 20:
-		message = "There is a visa appointment available on " + str(date) + ", " + str(diff) + " days before your trip to Barcelona. " 
-		sendMsg(diff, date, msg)
+        message = "There is a visa appointment available on " + \
+            str(date) + ", " + str(diff) + " days before your trip to Barcelona. "
+        send_msg(message)
 
-schedule.every(30).minutes.do(checkIfThereIsAnAvailability)
+
+schedule.every(30).minutes.do(check_for_availability)
 
 while True:
     schedule.run_pending()
     time.sleep(1)
+
